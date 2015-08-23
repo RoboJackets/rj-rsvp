a445414fdbc32d8a3648197d315059e9a4892c6d
Ryan Strat <ryanstrat@users.noreply.github.com>
2015-08-23 01:15:01 -0400

    Add counting query

--- index.php
+++ index.php
@@ -15,41 +15,41 @@ $app->get('/event/:code', function() use ($app) {
     $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
     if ($mysqli->connect_errno) {
         error_log("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
-        $app->request->setStatus(500);
+        $app->response->setStatus(500);
         return;
     }
 
     if (!($stmt = $mysqli->prepare('SELECT event_id FROM `events` WHERE CODE=? AND open<=CURDATE() AND close>=CURDATE()'))) {
         error_log("Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error);
-        $app->request->setStatus(500);
+        $app->response->setStatus(500);
         return;
     }
 
     if (!$stmt->bind_param("s", $code)) {
         error_log("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
-        $app->request->setStatus(500);
+        $app->response->setStatus(500);
         return;
     }
 
     if (!$stmt->execute()) {
         error_log("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
-        $app->request->setStatus(500);
+        $app->response->setStatus(500);
         return;
     }
 
     if(!$stmt->bind_result($eventID)){
         error_log("Bind Result failed: (" . $stmt->errno . ") " . $stmt->error);
-        $app->request->setStatus(500);
+        $app->response->setStatus(500);
         return;
     }
 
     $stmt->fetch();
+    
     echo "EventID:".$eventID;
 
     $stmt->close();
 
-    //Query if event exists & is between open and close date
-    //If yes, add click to database
+    $mysqli->query("INSERT INTO `rj-rsvp`.`clicks` (`click_id`, `event_id`, `ip_address`, `timestamp`) VALUES (NULL, '".$eventID."', INET_ATON(".$app->request->headers->get('x-forwarded-for');."), CURRENT_TIMESTAMP);");
 });
 
 $app->run();
